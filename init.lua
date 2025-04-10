184d235a90f23d911153aea3f16c262363c8057d
softyay <laylahoey22@gmail.com>
2025-04-10 04:29:09 -0400

    mini.files, revert some color things

--- init.lua
+++ init.lua
@@ -921,13 +921,11 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    --'folke/tokyonight.nvim',
-    'rebelot/kanagawa.nvim',
+    'folke/tokyonight.nvim',
+    -- 'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      --require('kanagawa').setup {
-      require('kanagawa').setup {
+      require('tokyonight').setup {
         transparent = true,
         styles = {
           sidebars = 'transparent',
@@ -939,8 +937,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      --vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'kanagawa'
+      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme ('kanagawa')
     end,
   },
 
@@ -964,6 +962,9 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+
+      -- Added by Layla
+      require('mini.files').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
