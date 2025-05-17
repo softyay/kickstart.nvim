64a117977b0c60e861aa6d5c0b7f8f7d925e7431
softyay <laylahoey22@gmail.com>
2025-05-17 19:01:17 -0400

    Comfortable lualine w tabline, winline

--- lua/custom/configs/softLualine.lua
+++ lua/custom/configs/softLualine.lua
@@ -1,8 +1,22 @@
-local M = {}
 local kanagawa_theme_builder = require 'custom.utils.softKanaLua'
 local lualine_theme = {}
 local lualine_separator_style = 'round'
+local standalone_separator_component = function(component_color, num_spaces)
+  return {
+    function()
+      local str = ''
+      for i in 1, num_spaces, 1 do
+        str = str .. ' '
+      end
+      return str
+    end,
+    color = component_color,
+    draw_empty = true,
+  }
+end
 
+-- Local fns
+-- ============================================================================
 local win_num = function()
   return 'win: ' .. vim.api.nvim_get_current_win()
 end
@@ -11,6 +25,23 @@ local buff_id = function()
   return 'buf: ' .. vim.api.nvim_get_current_buf()
 end
 
+local rename_wrapper = function()
+  local new_name = vim.fn.input 'New name: '
+  if vim.fn.exists ':LualineRename' == 1 then
+    vim.cmd.LualineRename(new_name)
+  end
+end
+
+local invert = function(sep_table)
+  return {
+    left = sep_table.right,
+    right = sep_table.left,
+  }
+end
+-- ============================================================================
+
+-- Separators
+-- ============================================================================
 -- stylua: ignore start
 local section_sep = function(style)
   if      style == 'slash'      then return { left = '', right = '' }
@@ -38,17 +69,221 @@ local component_sep = function(style)
   end
 end
 -- stylua: ignore end
---
-local dumdum = function()
-  return 'this is dumdum func'
-end
+-- ============================================================================
 
-local invert = function(sep_table)
-  return {
-    left = sep_table.right,
-    right = sep_table.left,
+-- Config function
+-- ============================================================================
+local config_fn = function()
+  -- local kana = require('kanagawa.colors').setup({ 'wave' }).palette
+  lualine_theme = kanagawa_theme_builder.get_lualine_theme {}
+
+  require('lualine').setup {
+    options = {
+      theme = lualine_theme,
+      icons_enabled = true,
+      component_separators = component_sep(lualine_separator_style),
+      section_separators = section_sep(lualine_separator_style),
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      always_show_tabline = true,
+      globalstatus = false,
+      refresh = {
+        statusline = 100,
+        tabline = 100,
+        winbar = 100,
+      },
+    },
+    sections = {
+      lualine_a = {
+        {
+          'mode',
+        },
+      },
+      lualine_b = {
+        {
+          'branch',
+        },
+        {
+          'diff',
+        },
+      },
+      lualine_c = {
+        {
+          'filename',
+          fmt = function(str)
+            return str:match '(.+)%..+$' or str
+          end,
+        },
+      },
+      lualine_x = {
+        {
+          'filetype',
+        },
+      },
+      lualine_y = {
+        {
+          'progress',
+        },
+      },
+      lualine_z = {
+        {
+          'location',
+        },
+      },
+    },
+    inactive_sections = {
+      lualine_a = {
+        {
+          'mode',
+        },
+      },
+      lualine_b = {
+        {
+          'branch',
+        },
+        {
+          'diff',
+        },
+      },
+      lualine_c = {
+        {
+          'filename',
+          fmt = function(str)
+            return str:match '(.+)%..+$' or str
+          end,
+        },
+      },
+      lualine_x = {
+        standalone_separator_component(nil, 0),
+        {
+          'filetype',
+        },
+      },
+      lualine_y = {
+        {
+          'progress',
+        },
+      },
+      lualine_z = {
+        {
+          'location',
+        },
+      },
+    },
+    tabline = {
+      lualine_a = {
+        {
+          'tabs',
+          mode = 2,
+          tabs_color = {
+            active = kanagawa_theme_builder.get_tab_colors().active,
+            inactive = kanagawa_theme_builder.get_tab_colors().inactive,
+          },
+        },
+      },
+      lualine_y = {
+        {
+          'windows',
+          use_mode_colors = true,
+          windows_color = {
+            active = kanagawa_theme_builder.get_tab_colors().active,
+            inactive = kanagawa_theme_builder.get_tab_colors().inactive,
+          },
+        },
+      },
+      lualine_z = {
+        {
+          'datetime',
+          color = lualine_theme.inactive.c,
+        },
+      },
+    },
+    winbar = {
+      lualine_a = {
+        {
+          'filename',
+        },
+      },
+      lualine_b = {
+        {
+          'lsp_status',
+        },
+      },
+      lualine_c = {
+        {
+          'diagnostics',
+          always_visible = true,
+        },
+      },
+      lualine_x = {
+        {
+          win_num,
+        },
+        {
+          buff_id,
+        },
+      },
+      lualine_y = {
+        {
+          'progress',
+        },
+      },
+      lualine_z = {
+        {
+          'location',
+        },
+      },
+    },
+    inactive_winbar = {
+      lualine_a = {
+        {
+          'filename',
+        },
+      },
+      lualine_b = {
+        {
+          'lsp_status',
+        },
+      },
+      lualine_c = {
+        {
+          'diagnostics',
+          colored = false,
+          always_visible = true,
+        },
+      },
+      lualine_x = {
+        {
+          win_num,
+        },
+        {
+          buff_id,
+        },
+      },
+      lualine_y = {
+        {
+          'progress',
+        },
+      },
+      lualine_z = {
+        {
+          'location',
+        },
+      },
+    },
+    extensions = {},
   }
+
+  -- create mappitg for renaming tabs
+  vim.keymap.set('n', '<leader>rt', function()
+    rename_wrapper()
+  end, { desc = 'Rename the current tab in LuaLine' })
 end
+-- ============================================================================
 
 return {
   'nvim-lualine/lualine.nvim',
@@ -57,209 +292,5 @@ return {
     'nvim-tree/nvim-web-devicons',
     require 'custom.configs.softKanagawa',
   },
-  config = function()
-    local kana = require('kanagawa.colors').setup({ 'wave' }).palette
-    lualine_theme = kanagawa_theme_builder.get_lualine_theme(kana)
-
-    require('lualine').setup {
-      options = {
-        theme = lualine_theme,
-        icons_enabled = true,
-        component_separators = component_sep(lualine_separator_style),
-        section_separators = section_sep(lualine_separator_style),
-        disabled_filetypes = {
-          statusline = {},
-          winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        always_show_tabline = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 100,
-          tabline = 100,
-          winbar = 100,
-        },
-      },
-      sections = {
-        lualine_a = {
-          {
-            'mode',
-          },
-        },
-        lualine_b = {
-          {
-            'branch',
-          },
-          {
-            'diff',
-          },
-        },
-        lualine_c = {
-          {
-            'filename',
-            fmt = function(str)
-              return str:match '(.+)%..+$' or str
-            end,
-          },
-        },
-        lualine_x = {
-          {
-            'filetype',
-          },
-        },
-        lualine_y = {
-          {
-            'progress',
-          },
-        },
-        lualine_z = {
-          {
-            'location',
-          },
-        },
-      },
-      inactive_sections = {
-        lualine_a = {
-          {
-            'mode',
-          },
-        },
-        lualine_b = {
-          {
-            'branch',
-          },
-          {
-            'diff',
-          },
-        },
-        lualine_c = {
-          {
-            'filename',
-            fmt = function(str)
-              return str:match '(.+)%..+$' or str
-            end,
-          },
-        },
-        lualine_x = {
-          {
-            function()
-              return ''
-            end,
-            draw_empty = true,
-          },
-          {
-            'filetype',
-          },
-        },
-        lualine_y = {
-          {
-            'progress',
-          },
-        },
-        lualine_z = {
-          {
-            'location',
-          },
-        },
-      },
-      tabline = {
-        lualine_a = {
-          {
-            'tabs',
-            mode = 0,
-            -- separator = invert(section_sep(lualine_separator_style)),
-            tabs_color = {
-              active = lualine_theme.inactive.a,
-              inactive = lualine_theme.inactive.b,
-            },
-          },
-        },
-        lualine_z = {
-          {
-            'windows',
-            use_mode_colors = false,
-            windows_color = {
-              active = lualine_theme.normal.a,
-              inactive = lualine_theme.inactive.b,
-            },
-          },
-        },
-      },
-      winbar = {
-        lualine_a = {
-          {
-            'filename',
-          },
-        },
-        lualine_b = {
-          {
-            'lsp_status',
-          },
-        },
-        lualine_c = {
-          {
-            'diagnostics',
-            always_visible = true,
-          },
-        },
-        lualine_x = {
-          {
-            win_num,
-          },
-          {
-            buff_id,
-          },
-        },
-        lualine_y = {
-          {
-            'progress',
-          },
-        },
-        lualine_z = {
-          {
-            'location',
-          },
-        },
-      },
-      inactive_winbar = {
-        lualine_a = {
-          {
-            'filename',
-          },
-        },
-        lualine_b = {
-          {
-            'lsp_status',
-          },
-        },
-        lualine_c = {
-          {
-            'diagnostics',
-            colored = false,
-            always_visible = true,
-          },
-        },
-        lualine_x = {
-          {
-            win_num,
-          },
-          {
-            buff_id,
-          },
-        },
-        lualine_y = {
-          {
-            'progress',
-          },
-        },
-        lualine_z = {
-          {
-            'location',
-          },
-        },
-      },
-      extensions = {},
-    }
-  end,
+  config = config_fn,
 }
--- lua/custom/utils/softKanaLua.lua
+++ lua/custom/utils/softKanaLua.lua
@@ -1,8 +1,20 @@
 local M = {}
 
-M.get_lualine_theme = function(palette)
-  local kana = palette
+M.get_tab_colors = function()
+  local kana = require('kanagawa.colors').setup({ 'wave' }).palette
   return {
+    active = { bg = kana.oldWhite, fg = kana.dragonBlack2 },
+    inactive = { bg = kana.dragonBlack4, fg = kana.dragonAsh },
+  }
+end
+
+M.get_lualine_theme = function(palette)
+  local kana = require('kanagawa.colors').setup({ 'wave' }).palette
+  -- local kana = palette
+  return {
+    -- NOTE: this style with the ash in the center still looks cool as hell...
+    -- just need to figure out how to make the tabline not need to obey it.
+    --
     -- normal = {
     --   a = { bg = kana.springBlue, fg = kana.dragonBlack2 },
     --   b = { bg = kana.dragonBlack4, fg = kana.springBlue },
@@ -11,45 +23,61 @@ M.get_lualine_theme = function(palette)
     --   y = { bg = kana.dragonBlack4, fg = kana.springBlue },
     --   z = { bg = kana.springBlue, fg = kana.dragonBlack2 },
     -- },
+    -- command = {
+    --   a = { bg = kana.roninYellow, fg = kana.dragonBlack2 },
+    --   b = { bg = kana.dragonBlack4, fg = kana.roninYellow },
+    --   c = { bg = kana.dragonAsh, fg = kana.dragonBlack4 },
+    --   x = { bg = kana.dragonAsh, fg = kana.dragonBlack4 },
+    --   y = { bg = kana.dragonBlack4, fg = kana.roninYellow },
+    --   z = { bg = kana.roninYellow, fg = kana.dragonBlack2 },
+    -- },
     normal = {
       a = { bg = kana.springBlue, fg = kana.dragonBlack2 },
       b = { bg = kana.dragonBlack4, fg = kana.springBlue },
-      c = { bg = kana.dragonBlack2, fg = kana.dragonAsh },
+      c = { bg = kana.dragonBlack2, fg = kana.springBlue },
       x = { bg = kana.dragonBlack2, fg = kana.dragonAsh },
-      y = { bg = kana.dragonBlack4, fg = kana.dragonAsh },
-      z = { bg = kana.dragonBlack5, fg = kana.dragonAsh },
+      y = { bg = kana.dragonBlack4, fg = kana.springBlue },
+      z = { bg = kana.dragonBlack5, fg = kana.springBlue },
     },
     insert = {
       a = { bg = kana.springGreen, fg = kana.dragonBlack2 },
       b = { bg = kana.dragonBlack4, fg = kana.springGreen },
-      c = { bg = kana.dragonBlack2, fg = kana.dragonAsh },
+      c = { bg = kana.dragonBlack2, fg = kana.springGreen },
       x = { bg = kana.dragonBlack2, fg = kana.dragonAsh },
-      y = { bg = kana.dragonBlack4, fg = kana.dragonAsh },
-      z = { bg = kana.dragonBlack5, fg = kana.dragonAsh },
+      y = { bg = kana.dragonBlack4, fg = kana.springGreen },
+      z = { bg = kana.dragonBlack5, fg = kana.springGreen },
     },
     visual = {
       a = { bg = kana.autumnRed, fg = kana.dragonBlack2 },
       b = { bg = kana.dragonBlack4, fg = kana.autumnRed },
-      c = { bg = kana.dragonAsh, fg = kana.dragonBlack4 },
-      x = { bg = kana.dragonAsh, fg = kana.dragonBlack4 },
-      y = { bg = kana.dragonBlack4, fg = kana.springGreen },
-      z = { bg = kana.springGreen, fg = kana.dragonBlack2 },
+      c = { bg = kana.dragonBlack2, fg = kana.autumnRed },
+      x = { bg = kana.dragonBlack2, fg = kana.dragonAsh },
+      y = { bg = kana.dragonBlack4, fg = kana.autumnRed },
+      z = { bg = kana.dragonBlack5, fg = kana.autumnRed },
     },
     replace = {
       a = { bg = kana.oniViolet, fg = kana.dragonBlack2 },
       b = { bg = kana.dragonBlack4, fg = kana.oniViolet },
-      c = { bg = kana.dragonAsh, fg = kana.dragonBlack4 },
-      x = { bg = kana.dragonAsh, fg = kana.dragonBlack4 },
-      y = { bg = kana.dragonBlack4, fg = kana.autumnRed },
-      z = { bg = kana.autumnRed, fg = kana.dragonBlack2 },
+      c = { bg = kana.dragonBlack2, fg = kana.oniViolet },
+      x = { bg = kana.dragonBlack2, fg = kana.dragonAsh },
+      y = { bg = kana.dragonBlack4, fg = kana.oniViolet },
+      z = { bg = kana.dragonBlack5, fg = kana.oniViolet },
     },
     command = {
       a = { bg = kana.roninYellow, fg = kana.dragonBlack2 },
       b = { bg = kana.dragonBlack4, fg = kana.roninYellow },
-      c = { bg = kana.dragonAsh, fg = kana.dragonBlack4 },
-      x = { bg = kana.dragonAsh, fg = kana.dragonBlack4 },
+      c = { bg = kana.dragonBlack2, fg = kana.roninYellow },
+      x = { bg = kana.dragonBlack2, fg = kana.dragonAsh },
       y = { bg = kana.dragonBlack4, fg = kana.roninYellow },
-      z = { bg = kana.roninYellow, fg = kana.dragonBlack2 },
+      z = { bg = kana.dragonBlack5, fg = kana.roninYellow },
+    },
+    terminal = {
+      a = { bg = kana.fujiWhite, fg = kana.dragonBlack2 },
+      b = { bg = kana.dragonBlack4, fg = kana.fujiWhite },
+      c = { bg = kana.dragonBlack2, fg = kana.fujiWhite },
+      x = { bg = kana.dragonBlack2, fg = kana.dragonAsh },
+      y = { bg = kana.dragonBlack4, fg = kana.fujiWhite },
+      z = { bg = kana.dragonBlack5, fg = kana.fujiWhite },
     },
     inactive = {
       a = { bg = kana.dragonBlack5, fg = kana.dragonAsh, gui = 'italic' },
