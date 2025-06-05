34057165cbc221751d057fade423a72a50c56271
softyay <laylahoey22@gmail.com>
2025-06-04 21:23:47 -0400

    .meta file filter

--- lua/custom/configs/softMiniFile.lua
+++ lua/custom/configs/softMiniFile.lua
@@ -1,5 +1,90 @@
 local maxWindows = 3
 
+-- Filter class
+local Filter = {
+  name = 'Unnamed Filter',
+  enabled = false,
+  should_show_entry = function(entry)
+    return true
+  end,
+  show_toggle_messages = true,
+  show_debug_messages = true,
+}
+Filter.__index = Filter
+
+local filter_calls = 0
+
+function Filter:toggle()
+  self.enabled = not self.enabled
+  if self.show_toggle_messages then
+    local result = self.enabled and 'on' or 'off'
+    print(self.name .. ' filter toggled ' .. result)
+  end
+end
+
+--stylua: ignore start
+function Filter:get_callback()
+  if (self.enabled) then
+    return self.should_show_entry
+  else
+    return function(entry)
+      return true
+    end
+  end
+end
+--stylua: ignore end
+
+function Filter.new(name, start_enabled, should_show_entry)
+  local self = setmetatable({}, Filter)
+  self.name = name
+  self.enabled = start_enabled
+  self.should_show_entry = should_show_entry
+  return self
+end
+
+-- .meta filtering
+local meta_filter = Filter.new('Meta Files', false, function(entry)
+  return not vim.endswith(entry.name, '.meta')
+end)
+
+local filters = {
+  meta_filter,
+}
+
+local disable_all = false
+local passes_all_filters = function(entry)
+  filter_calls = filter_calls + 1
+
+  for i = 1, #filters do
+    local current_filter = filters[i]
+    local passes_current_filter = current_filter:get_callback()
+
+    if disable_all then
+      current_filter.show_debug_messages = false
+    end
+
+    if not passes_current_filter(entry) then
+      if current_filter.show_debug_messages then
+        local prompt = "Entry '" .. entry .. "' was filtered by filter " .. i .. ' (' .. filters[i].name .. ') \n'
+        prompt = prompt .. 'Enter -> continue\n'
+        prompt = prompt .. "'off' -> disable debug mode for this filter\n"
+        prompt = prompt .. "'all off' -> disable debug mode off for all filters\n"
+        local inp = vim.fn.input(prompt)
+        if inp == 'off' then
+          current_filter.show_debug_messages = false
+        elseif inp == 'all off' then
+          current_filter.show_debug_messages = false
+          disable_all = true
+        end
+      end
+
+      return false
+    end
+  end
+
+  return true
+end
+
 return {
   'echasnovski/mini.files',
   config = function()
@@ -8,7 +93,7 @@ return {
       -- Customization of shown content
       content = {
         -- Predicate for which file system entries to show
-        filter = nil,
+        filter = passes_all_filters,
         -- What prefix to show to the left of file system entry
         prefix = nil,
         -- In which order to show file system entries
@@ -67,6 +152,17 @@ return {
     vim.keymap.set('n', '<leader>ef', function()
       minifiles.open(vim.fn.getreg '%', false)
     end, { desc = '[E]xplore from [F]ile' })
+
+    vim.keymap.set('n', '<leader>etm', function()
+      meta_filter:toggle()
+      MiniFiles.refresh { content = { filter = passes_all_filters } }
+    end, { desc = '[E]xplore: [T]oggle .[m]eta files' })
+    -- vim.api.nvim_create_autocmd('User', {
+    --   pattern = 'MiniFilesWindowOpen',
+    --   callback = function(args)
+    --     MiniFiles.refresh { content = { filter = filter_through_enabled } }
+    --   end,
+    -- })
 
     -- TODO: Make a keymap to find the current directory under the cursor
     -- possibly check for a git repo first.
